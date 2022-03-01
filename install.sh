dec7a4d793767dde073a95cfd75629d4977bd523
Jon Ferguson <jon@jef.us>
2022-03-01 16:32:10 -0500

    added missing install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [[ "${BASH_SOURCE-}" != "$0" ]]; then
     exit 33
 fi
 
-# FIXME make sure we are in the right directory beofre proceeding
+# FIXME make sure we are in the right directory before proceeding
 
 git clone https://github.com/Homebrew/brew homebrew
 cd homebrew
