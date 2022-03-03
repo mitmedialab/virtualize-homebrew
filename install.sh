f8d5772d981886764d89bc44175a3f962432190d
Jon Ferguson <jon@jef.us>
2022-03-03 17:12:21 -0500

    splitting the brew clone dir and the install dir

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ fi
 VIRTUALIZE_HOMEBREW_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE}" )" &> /dev/null && pwd )
 
 cd $VIRTUALIZE_HOMEBREW_DIR
-git clone https://github.com/Homebrew/brew homebrew
-cd homebrew
-bin/brew config
+git clone https://github.com/Homebrew/brew
+cd brew
+mkdir $VIRTUALIZE_HOMEBREW_DIR/homebrew
+bin/brew config --prefix $VIRTUALIZE_HOMEBREW_DIR/homebrew
+#rm -rf $VIRTUALIZE_HOMEBREW_DIR/brew  # FIXME uncomment when done debugging
 bin/brew install the_silver_searcher
 
 echo "brew installed"
@@ -21,9 +23,5 @@ exit
 ## Notes:
 ## we might be able to just do normal install and use the `brew config --prefix` command
 ## instead of cloning and building from the repo
-## rename homebrew install dir to just brew (to match the repo name)?
 ## don't forget the .gitignore dir if ^^^
-## are man pages a thing?
-## if we do keep installing by cloning the repo, colne the repo into one dir,
-## then install into another dir and removing the cloned repo dir?
 ## maybe we should have a list of brew packages that need to be installed for a given project?
